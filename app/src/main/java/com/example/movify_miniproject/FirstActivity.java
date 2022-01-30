37db654dd06fcf1fb21bd7e64fcae46b547c36d1
Aditi Desai <71864212+aditidesai27@users.noreply.github.com>
2022-01-30 13:15:09 +0530

    Splash Screen

---
+++ app/src/main/java/com/example/movify_miniproject/FirstActivity.java
@@ -0,0 +1,19 @@
+package com.example.movify_miniproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+        setContentView(R.layout.activity_first);
+    }
+}
