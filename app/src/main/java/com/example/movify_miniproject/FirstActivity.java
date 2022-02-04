8b12d618372f2efa3d4f9f51cede4346f214e840
sarthakdharia <71364213+sarthakdharia@users.noreply.github.com>
2022-02-04 12:12:38 +0530

    login

--- app/src/main/java/com/example/movify_miniproject/FirstActivity.java
+++ app/src/main/java/com/example/movify_miniproject/FirstActivity.java
@@ -2,18 +2,32 @@ package com.example.movify_miniproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class FirstActivity extends AppCompatActivity {
+    private ImageButton button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
         setContentView(R.layout.activity_first);
+        button = (ImageButton) findViewById(R.id.login_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openLoginActivity();
+            }
+        });
+    }
+
+    public void openLoginActivity(){
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
     }
 }
