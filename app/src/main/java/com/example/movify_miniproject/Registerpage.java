6d98998df6061c6232153f2fa5fc004218c3194c
Aditi Desai <71864212+aditidesai27@users.noreply.github.com>
2022-02-07 11:54:47 +0530

    updated xml files

--- app/src/main/java/com/example/movify_miniproject/Registerpage.java
+++ app/src/main/java/com/example/movify_miniproject/Registerpage.java
@@ -26,7 +26,6 @@ public class Registerpage extends AppCompatActivity {
     @Override
     public void onStart() {
         super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if(currentUser != null){
             currentUser.reload();
